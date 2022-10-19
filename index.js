c82fd80235380d4f6583060592cb20837ca4cffd
Marina Nakvas <60181995+marfuny51@users.noreply.github.com>
2022-10-19 19:31:55 +0200

    context examples

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
 // function declaration
-
-function square(number) {
-  return number * number;
+function summFD(a, b) {
+  return a + b;
 }
 
-
 // function expression
+const summFE = function(a, b) {
+  return a + b;
+};
+const name = summFE(4);
 
-// simple example
-const square = function(number) { return number * number; };
-const name = square(4);
+// arrow function
+const double = a => 2 * a;
+const summFA = (a, b) => {
+  return a + b;
+};
 
-// return global object window
-// if 'use strict' return undefined
+// global
+console.log(this); // return global window object
+
+// function
+function functionDeclaration() {
+  return this; // return global window object // if 'use strict' return undefined
+}
+
 (function() {
-  return this;
+  return this; // return global window object // if 'use strict' return undefined
 })();
+
+const f = () => {
+  return this; // return global window object // if 'use strict' also
+}; 
+
+// object
+const functionExpression = {
+  count: 10,
+  showCount: function() {
+    return this.count;
+  }
+}
+functionExpression.showCount(); // return 10, this = Object {...}
+
+const functionArrow = {
+  count: 10,
+  showCount: () => {
+    return this.count;
+  },
+}
+functionArrow.showCount(); // return undefined, this = global window object
+
+// class
+function ClassFE() {
+  this.age = 0;
+
+  setInterval(function() {
+    this.age++; // this = global window object
+  }, 1000);
+}
+const newFE = new ClassFE();
+
+function ClassFA() {
+  this.age = 0;
+
+  setInterval(() => {
+    this.age++; // this = ClassFA {...}
+  }, 1000);
+}
+const newFA = new ClassFA();
+
+// loop
+const jow = [5, 10, 45, 67];
+
+jow.forEach(function(v, i){
+  // console.log(this); // this = global window object 
+    this[i] = v + 1;
+});
+
+jow.forEach(function(v, i){
+    this[i] = v + 1;
+    // console.log(this); // this = jow 
+}, jow);
+
+jow.forEach((v, i) => {
+  // console.log(this); // this = global window object
+  return this[i] = v + 1;
+});
+
+jow.forEach((v, i) => {
+  console.log(this); // this = global window object
+  return this[i] = v + 1;
+}, jow);
+
+
 
 // example with this
 this.x = 9;
@@ -28,43 +103,5 @@ const getX = module.getX;
 getX(); // 9, поскольку в этом случае this ссылается на глобальный объект
 const boundGetX = getX.bind(module); // создаём новую функцию с this, привязанным к module
 boundGetX(); // 81
-
-const obj1 = {
-  i: 10,
-  c: function() {
-    console.log(this.i, this);
-  }
-}
-obj1.c(); // prints 10, Object {...}
-
-
-
-// arrow function
-
-// simple example
-const func1 = x => x * x;
-const func2 = (x, y) => { return x + y; };
-
-function Person() {
-  this.age = 0;
-
-  setInterval(() => {
-    this.age++; // `this` указывает на объект Person
-  }, 1000);
-}
-
-const p = new Person();
-
-
-const f = () => { 'use strict'; return this; };
-console.log(f()); // return global object window
-
-
-var obj = {
-  i: 10,
-  b: () => console.log(this.i, this),
-}
-obj.b(); // prints undefined, Window {...} (или глобальный объект)
-
 
 //Функции-генераторы
